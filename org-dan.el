c5429ddb6e42342230dc26854a0e0de25be86c3b
Dan Davison <davison@stats.ox.ac.uk>
2009-07-15 14:00:17 -0400

    Org-style folding via ;;* ;;** etc
    From Tassilo Horn / Michael Zeller / Seb Vauban

--- org-dan.el
+++ org-dan.el
@@ -1,7 +1,9 @@
-;; Org-mode settings
+;;* Org-mode settings
 
 (add-to-list 'load-path "/usr/local/src/org-mode/lisp")
 (require 'org-install)
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
 (add-to-list 'load-path "/usr/local/src/org-mode/contrib/lisp")
 (require 'org-mairix)
 
@@ -13,6 +15,11 @@
 (load "~/src/org/org-util.el")
 (load "~/src/org/org-R/org-R.el")
 
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+;;* org-babel
 (add-to-list 'load-path "~/src/org-babel/lisp")
 (require 'org-babel-init)
 (setq org-babel-tangle-langs
@@ -20,30 +27,33 @@
 	("python" . ("py" "#!/usr/bin/env python"))
 	("R" . ("R" "#!/usr/bin/env R"))
 	("sh" . ("sh" "#!/usr/bin/env bash"))
-	))
+	("emacs-lisp" . ("el" ""))))
 			      
-
+;;* export latex
+(setq org-export-latex-classes nil)
+(add-to-list 'org-export-latex-classes
+	     '("beamer"
+	       "\\documentclass[11pt]{beamer}\n\\usepackage[utf8]{inputenc}\n\\usepackage[T1]{fontenc}\n\\usepackage{hyperref}\n\\usepackage{verbatim}\n\\setbeameroption{show notes}\n\\usetheme{Goettingen}\n\\useoutertheme{infolines}"
+	       ("\\begin{frame}\\frametitle{%s}\\begin{itemize}" "\\end{itemize}\\end{frame}"
+		"\\begin{frame}\\frametitle{%s}\\begin{itemize}" "\\end{itemize}\\end{frame}")
+	       ("\\item{%s}" . "\\item*{%s}")
+	       ("\\end{itemize}\\note{" "}")))
 
 (load "~/website/website.el")
 
-(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
-(setq org-dan-expert t)
+;; (setq org-dan-expert t)
 (defun dan-org-mode-hook ()
   (mapc (lambda (pair) (local-set-key (car pair) (cdr pair)))
 	dan-org-mode-keybindings))
 (add-hook 'org-mode-hook 'dan-org-mode-hook)
 
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
 
 ;; for color-theme-charcoal-black (can this be done programatically?)
 
 (add-hook 'org-src-mode-hook '(lambda () (outline-minor-mode nil)))
 
 
-;;;
-;;; agenda
+;;* agenda
 (org-defkey org-agenda-mode-map [(right)] 'forward-char)
 (org-defkey org-agenda-mode-map [(left)] 'backward-char)
 
@@ -73,7 +83,7 @@
 (setq org-directory "~/org")
 (setq org-agenda-files (list org-directory))
 (setq org-agenda-start-on-weekday nil)
-(setq org-agenda-ndays 7)
+(setq org-agenda-ndays 30)
 (setq org-agenda-compact-blocks t)
 (setq org-deadline-warning-days 7)
 ;; (set-face-foreground 'org-agenda-date-weekend "red")
@@ -82,13 +92,50 @@
 (setq org-agenda-custom-commands
       '(
 	("W" "Search for work items in state" todo "x"
-	 ((org-agenda-files '("~/org/work.org"))))
+	 ((org-agenda-files '("~/org/work.org"
+			      "~/org/wtccc2.org"
+			      "~/org/pobi.org"
+			      "~/org/shellfish.org"))))
 	("T" "Search for tasks items in state" todo "x"
 	 ((org-agenda-files '("~/org/tasks.org"))))
 	("C" "Search for computing items in state" todo "x"
 	 ((org-agenda-files '("~/org/computing.org"))))
 	))
-;;; remember
+
+(defun org-agenda-format-date-aligned-dan (date)
+  "Dan's modified version of `org-agenda-format-date-aligned'.
+
+Format a date string for display in the daily/weekly agenda, or
+timeline.  This function makes sure that dates are aligned for
+easy reading.
+"
+  (require 'cal-iso)
+  (let* ((dayname (calendar-day-name date))
+	 (day (cadr date))
+	 (day-of-week (calendar-day-of-week date))
+	 (month (car date))
+	 (monthname (calendar-month-name month))
+	 (year (nth 2 date))
+	 (iso-week (org-days-to-iso-week
+		    (calendar-absolute-from-gregorian date)))
+	 (weekyear (cond ((and (= month 1) (>= iso-week 52))
+			  (1- year))
+			 ((and (= month 12) (<= iso-week 1))
+			  (1+ year))
+			 (t year)))
+	 (weekstring (if (= day-of-week 1)
+			 (format " W%02d" iso-week)
+		       "")))
+;;;     (format "%-10s %2d %s %4d%s"
+;;; 	    dayname day monthname year weekstring)
+    
+    (format "%s %2d %s"
+	    (substring dayname 0 3) day (substring monthname 0 3))))
+
+(setq org-agenda-format-date 'org-agenda-format-date-aligned-dan)
+
+
+;;* remember
 (org-remember-insinuate)
 (setq org-default-notes-file "~/org/etc.org")
 ;; (setq org-remember-default-headline "top")
@@ -131,44 +178,72 @@ may use one of several remember targets"
 (setq org-hide-leading-stars t)
 ;; (setq org-show-entry-below t) not sure why I had this
 (set-face-attribute 'org-hide nil :foreground "gray15")
+;; (set-face-attribute 'org-hide nil :foreground "white")
 (setq org-export-with-LaTeX-fragments t)
 
-;; personal alteration
-;; This sort of thing should probably be maintained using git somehow
-;; there's a FAQ on the org web page about that
-(defun org-agenda-format-date-aligned-dan (date)
-  "Dan's modified version of `org-agenda-format-date-aligned'.
+;;* refiling
+;; http://doc.norang.ca/org-mode.html#Refiling
 
-Format a date string for display in the daily/weekly agenda, or
-timeline.  This function makes sure that dates are aligned for
-easy reading.
-"
-  (require 'cal-iso)
-  (let* ((dayname (calendar-day-name date))
-	 (day (cadr date))
-	 (day-of-week (calendar-day-of-week date))
-	 (month (car date))
-	 (monthname (calendar-month-name month))
-	 (year (nth 2 date))
-	 (iso-week (org-days-to-iso-week
-		    (calendar-absolute-from-gregorian date)))
-	 (weekyear (cond ((and (= month 1) (>= iso-week 52))
-			  (1- year))
-			 ((and (= month 12) (<= iso-week 1))
-			  (1+ year))
-			 (t year)))
-	 (weekstring (if (= day-of-week 1)
-			 (format " W%02d" iso-week)
-		       "")))
-;;;     (format "%-10s %2d %s %4d%s"
-;;; 	    dayname day monthname year weekstring)
-    
-    (format "%s %2d %s"
-	    (substring dayname 0 3) day (substring monthname 0 3))))
+;; Use IDO for target completion
+(setq org-completion-use-ido t)
 
-(setq org-agenda-format-date 'org-agenda-format-date-aligned-dan)
+;; Targets include this file and any file contributing to the agenda - up to 5 levels deep
+(setq org-refile-targets (quote ((org-agenda-files :maxlevel . 5) (nil :maxlevel . 5))))
+
+;; Targets start with the file name - allows creating level 1 tasks
+(setq org-refile-use-outline-path 'file)
+
+;; Targets complete in steps so we start with filename, TAB shows the next level of targets etc 
+(setq org-outline-path-complete-in-steps t)
+
+;;* folding
+;; [[gnus:org#87zlb6vt8m.fsf@mundaneum.com][Email from Sébastien Vauban: {Orgmode} Re: org-style foldin]]
+(defun prompt-for-outline-regexp (new-regexp)
+  "ask the user for a local value of outline-regexp in this buffer"
+  (interactive "Outline regexp: ")
+  (set (make-local-variable 'outline-regexp) new-regexp)
+)
+
+(global-set-key (kbd "<f9>") 'prompt-for-outline-regexp)
+
+(defun th-outline-regexp ()
+ "Calculate the outline regexp for the current mode."
+ (let ((comment-starter (replace-regexp-in-string
+                         "[[:space:]]+" "" comment-start)))
+   (when (string= comment-start ";")
+     (setq comment-starter ";;"))
+   (concat "^" comment-starter "\\*+")))
+
+(defun th-outline-minor-mode-init ()
+  (interactive)
+  (setq outline-regexp (th-outline-regexp))
+
+  ;; highlight the headings
+  ;; see http://www.gnu.org/software/emacs/manual/html_node/emacs/Font-Lock.html
+  ;; use M-x customize-apropos face to customize faces
+  ;; to find the corresponding face for each outline level see org-faces.el
+  (let ((heading-1-regexp (concat (substring outline-regexp 0 -1) "\\{1\\} \\(.*\\)"))
+        (heading-2-regexp (concat (substring outline-regexp 0 -1) "\\{2\\} \\(.*\\)"))
+        (heading-3-regexp (concat (substring outline-regexp 0 -1) "\\{3\\} \\(.*\\)"))
+        (heading-4-regexp (concat (substring outline-regexp 0 -1) "\\{4,\\} \\(.*\\)"))
+        )
+    (font-lock-add-keywords
+     nil
+     `((,heading-1-regexp 1 'org-level-1 t)
+       (,heading-2-regexp 1 'org-level-2 t)
+       (,heading-3-regexp 1 'org-level-3 t)
+       (,heading-4-regexp 1 'org-level-4 t)))))
+
+(add-hook 'outline-minor-mode-hook
+          'th-outline-minor-mode-init)x
 
 
+;; (org-level-1 ((t (:foreground "cornflower blue" :weight bold :height 1.8 :family "Arial"))))
+;; (org-level-2 ((t (:foreground "LimeGreen" :weight bold :height 1.6 :family "Arial"))))
+;; (org-level-3 ((t (:foreground "orange" :weight bold :height 1.3 :family "Arial"))))
+
+
+;;* Extra org functions
 
 (defun org-insert-link-maybe ()
   "insert a file link depending on the context"
@@ -181,3 +256,24 @@ easy reading.
       nil)))
 
 (add-hook 'org-tab-first-hook 'org-insert-link-maybe)
+
+(defun org-dan-read-subtrees ()
+  "Return subtrees as a list of strings"
+  (let ((subtrees))
+    (while (or (looking-at "^*") (outline-next-heading))
+      (outline-mark-subtree)
+      (setq subtrees (cons (buffer-substring (point) (mark)) subtrees))
+      (goto-char (mark)))
+    (nreverse subtrees)))
+
+(defun org-dan-reverse-subtrees ()
+  "Reverse the order of all subtrees.
+
+Should start by setting restriction?
+"
+  (interactive)
+  (beginning-of-line)
+  (let ((subtrees (org-dan-read-subtrees)))
+    (beginning-of-buffer)
+    (delete-region (point) (mark))
+    (insert (mapconcat 'identity (nreverse subtrees) "\n"))))
