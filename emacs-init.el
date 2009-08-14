9257b0c32e65512575effb2e38d0c5a7543e99a9
Dan Davison <davison@stats.ox.ac.uk>
2009-08-14 12:42:19 -0400

    Update safe-local-variables in emacs-init.el customization code.

--- emacs-init.el
+++ emacs-init.el
@@ -10,7 +10,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((org-babel-default-header-args (:results . "output") (:session . "*R*")) (noweb-default-code-mode . R-mode) (outline-minor-mode)))))
+ '(safe-local-variable-values (quote ((org-babel-default-header-args (:results . "replace output") (:session . "*R*") (:exports . "none")) (noweb-default-code-mode . R-mode) (outline-minor-mode)))))
 (if nil
     (custom-set-faces
      ;; custom-set-faces was added by Custom.
@@ -18,6 +18,7 @@
      ;; Your init file should contain only one such instance.
      ;; If there is more than one, they won't work right.
      '(default ((t (:inherit nil :stipple nil :background "Grey15" :foreground "Grey" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))))
+
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
