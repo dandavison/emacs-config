11dc87392d0f1c9ef735c141b97da2c8b3cc3503
Dan Davison <davison@stats.ox.ac.uk>
2010-03-31 15:27:29 -0400

    Load some babel languages early for use during org-babel-load-file

--- emacs-init.el
+++ emacs-init.el
@@ -2,6 +2,12 @@
 (add-to-list 'load-path "/usr/local/src/emacs/org-mode/contrib/lisp")
 (require 'org-install)
 (require 'org-babel-init)
+(setq dan/org-babel-languages
+      '(R python ruby ditaa latex asymptote dot sass perl sql gnuplot clojure octave matlab C))
+(setq dan/org-babel-early-load-languages
+      '(R))
+(mapc '(lambda (lang) (require (intern (format "org-babel-%s" lang))))
+      dan/org-babel-early-load-languages)
 (org-babel-load-file "~/config/emacs/emacs.org")
 ;; (custom-set-variables
 ;;   ;; custom-set-variables was added by Custom.
