e5e08951fd5665dea5ff76178fe6f459c74a8b10
Dan Davison <davison@stats.ox.ac.uk>
2010-04-12 19:16:19 -0400

    Tidy up keys file

--- keys-dan.el
+++ keys-dan.el
@@ -1,11 +1,5 @@
 (setq dan/global-keybindings
-      '(
-	; ("\"" . dan/insert-double-quotes)
-	; ("(" . insert-parentheses)
-	; ("[" . dan/insert-square-brackets)
-	; ("{" . dan/insert-curly-brackets)
-	;; (global-set-key (kbd "[") #'insert-pair) use this strategy instead?
-	("\C-x\C-b" . org-buffers-list)
+      '(("\C-x\C-b" . org-buffers-list)
 	("\C-n" . dan/next-line-and-indent)
 	("\C-p" . dan/previous-line-and-indent)
 	("\C-ca" . org-agenda)
@@ -18,9 +12,6 @@
 	("\C-cs" . search-forward-symbol-at-point)
 	("\C-cv" . revert-buffer)
 	("\C-\M-g" . lgrep)
-	;; ("\C-c\C-r" . budget-eval)
-	;; ("\C-c\C-w" . dan/xclip-kill)
-	;; ("\C-c\C-y" . dan/xclip-yank)
 	([(control next)] . end-of-buffer)
 	([(control prior)] . beginning-of-buffer) 
 	([(s tab)] . lisp-complete-symbol) 
@@ -32,7 +23,6 @@
 	([(meta right)] . winner-redo)
 	([f1] . org-buffers-list)
 	([f2] . (lambda () (interactive) (switch-to-buffer "*Group*")))
-	;; ([f2] . dan/gnus-get-mail)
 	([f3] . (lambda () (interactive) (switch-to-buffer "*Org Agenda*")))
 	([f4] . (lambda () (interactive) (switch-to-buffer "*shell*")))
 	([f5] . (lambda () (interactive) (switch-to-buffer "*Python*")))
@@ -42,34 +32,20 @@
 	([f10] . delete-other-windows)
 	([f11] . delete-window)
 	([delete] . kill-buffer)
-	([escape] . other-window)
-	;; ("\C-q" . isearch-exit)
-	;; ("\C-cf" . font-lock-fontify-buffer)
-	;; ("\C-ci" . indent-buffer)
-	))
+	([escape] . other-window)))
 
 (setq dan/ess-and-iess-keybindings
-      '(
-	;; ("\"" . dan/insert-double-quotes)
-	;; ("[" . dan/insert-square-brackets)
-	;; ("{" . dan/insert-curly-brackets)
-	;; ("(" . dan/ess-insert-parentheses)
-	;; ("-" . ess-smart-underscore)
-	;; ("_" . dan/insert--)
-	("\C-c?" . ess-display-help-on-object)
+      '(("\C-c?" . ess-display-help-on-object)
 	("\C-ca" . ess-r-args-show)
 	("\C-cd" . dan/ess-list-R-function-definitions)
 	("\C-cf" . dan/ess-insert-function-template)
 	("\C-ck" . dan/ess-kill-line-and-indent)
 	("\C-cx" . dan/ess-recover-R-process)
 	([(control return)] . ess-eval-line-and-step)
-	([(shift tab)] . ess-complete-object-name)
-        ;; ([tab] . comint-dynamic-complete)
-	))
+	([(shift tab)] . ess-complete-object-name)))
 
 (setq dan/org-mode-keybindings
-      '(
-	([(control down)] . org-insert-heading)
+      '(([(control down)] . org-insert-heading)
 	([(control left)] . org-metaleft)
 	([(control right)] . org-metaright)
 	([(meta left)] . winner-undo)
@@ -82,10 +58,9 @@
 
 (setq dan/emacs-lisp-mode-keybindings
       '(("\C-cd" . edebug-defun)))
+
 (setq dan/c-c++-mode-keybindings
-      '(
-	;; ("(" . insert-parentheses)
-	("\C-c\C-l" . dan/compile-and-switch-to-iESS)))
+      '(("\C-c\C-l" . dan/compile-and-switch-to-iESS)))
 
 (setq dan/python-mode-keybindings
       '(()))
@@ -96,10 +71,8 @@
 	("\C-c=" . reftex-toc-dan)))
 
 (setq dan/paste-mode-keybindings
-      '(
-	("\"" . self-insert-command)
+      '(("\"" . self-insert-command)
 	("(" . self-insert-command)
 	("[" . self-insert-command)
 	("-" . self-insert-command)
-	("_" . self-insert-command)
-	))
+	("_" . self-insert-command)))
