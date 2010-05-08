e50ca81e2456e1d1be7558d6c7b6df33e6286a10
Dan Davison <davison@stats.ox.ac.uk>
2010-05-08 00:33:11 -0400

    Don't load starter kit

--- emacs-init.el
+++ emacs-init.el
@@ -10,3 +10,7 @@
       dan/org-babel-early-load-languages)
 (setq max-mini-window-height nil)
 (org-babel-load-file "~/config/emacs/emacs.org")
+
+;; (add-to-list 'load-path "~/emacs/emacs-starter-kit")
+;; (load-file "~/emacs/emacs-starter-kit/init.el")
+;; (org-babel-load-file "~/emacs/emacs-starter-kit/starter-kit.org")
