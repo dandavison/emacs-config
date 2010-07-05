e8a1ca8d17066856fd5a88349e2130825c9bf2f6
Dan Davison <davison@stats.ox.ac.uk>
2010-07-05 15:57:38 -0400

    New org-babel configuration

--- emacs-init.el
+++ emacs-init.el
@@ -1,12 +1,10 @@
 (add-to-list 'load-path "/usr/local/src/emacs/org-mode/lisp")
-(add-to-list 'load-path "/usr/local/src/emacs/org-mode/contrib/lisp")
 (require 'org-install)
-(require 'org-babel-init)
 (setq dan/org-babel-languages
       '(R python ruby ditaa latex asymptote dot sass perl sql gnuplot clojure octave matlab C))
 (setq dan/org-babel-early-load-languages
       '(R))
-(mapc '(lambda (lang) (require (intern (format "org-babel-%s" lang))))
+(mapc '(lambda (lang) (require (intern (format "ob-%s" lang))))
       dan/org-babel-early-load-languages)
 (setq max-mini-window-height nil)
 (org-babel-load-file "~/config/emacs/emacs.org")
