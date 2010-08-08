04801f4fa4d17b96acf6377dcea99eeb61ee150f
Dan Davison <davison@stats.ox.ac.uk>
2010-08-07 21:41:15 -0400

    Mysterious custom-set variable

--- emacs-init.el
+++ emacs-init.el
@@ -12,3 +12,9 @@
 ;; (add-to-list 'load-path "~/emacs/emacs-starter-kit")
 ;; (load-file "~/emacs/emacs-starter-kit/init.el")
 ;; (org-babel-load-file "~/emacs/emacs-starter-kit/starter-kit.org")
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(canlock-password "c6d84111c8952c110a560f52e80fdd8dc62bcf1d"))
