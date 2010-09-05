39df9a33fd8225138e14f2aeead180acaaf26b9d
Dan Davison <davison@stats.ox.ac.uk>
2010-09-05 19:42:11 -0400

    Update

--- emacs-init.el
+++ emacs-init.el
@@ -13,8 +13,15 @@
 ;; (load-file "~/emacs/emacs-starter-kit/init.el")
 ;; (org-babel-load-file "~/emacs/emacs-starter-kit/starter-kit.org")
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(canlock-password "c6d84111c8952c110a560f52e80fdd8dc62bcf1d"))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(canlock-password "c6d84111c8952c110a560f52e80fdd8dc62bcf1d")
+ '(safe-local-variable-values (quote ((varname . value) (noweb-default-code-mode . R-mode) (outline-minor-mode)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
