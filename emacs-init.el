1ae4c49cb8a5a7206a41d56fe6eca22ef4e7cfe1
Dan Davison <dandavison7@gmail.com>
2010-11-04 16:28:17 +0000

    Update emacs-init

--- emacs-init.el
+++ emacs-init.el
@@ -9,15 +9,20 @@
 ;; (load-file "~/emacs/emacs-starter-kit/init.el")
 ;; (org-babel-load-file "~/emacs/emacs-starter-kit/starter-kit.org")
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(canlock-password "c6d84111c8952c110a560f52e80fdd8dc62bcf1d")
- '(safe-local-variable-values (quote ((varname . value) (noweb-default-code-mode . R-mode) (outline-minor-mode)))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+ '(safe-local-variable-values (quote ((org-export-latex-image-default-option . "width=0.4\\textwidth") (org-export-latex-listings . t) (varname . value) (noweb-default-code-mode . R-mode) (outline-minor-mode)))))
+
+
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
