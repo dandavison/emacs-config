11ee4e8dc917c3636794676b886c63aac8042dee
Dan Davison <davison@Dan-Davisons-MacBook-Pro.local>
2010-11-20 18:00:21 +0000

    Get rid of safe-local-variables rubbish

--- emacs-init.el
+++ emacs-init.el
@@ -9,12 +9,3 @@
 ;; (add-to-list 'load-path "~/emacs/emacs-starter-kit")
 ;; (load-file "~/emacs/emacs-starter-kit/init.el")
 ;; (org-babel-load-file "~/emacs/emacs-starter-kit/starter-kit.org")
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(canlock-password "c6d84111c8952c110a560f52e80fdd8dc62bcf1d")
- '(safe-local-variable-values (quote ((org-export-latex-image-default-option . "width=0.4\\textwidth") (org-export-latex-listings . t) (varname . value) (noweb-default-code-mode . R-mode) (outline-minor-mode)))))
-
-
