fdf06df77932e2e77a42036ef43898289e9c5a49
Dan Davison <dandavison7@gmail.com>
2011-01-28 21:12:37 +0000

    custom set variables

--- emacs-init.el
+++ emacs-init.el
@@ -10,4 +10,15 @@
 ;; (load-file "~/emacs/emacs-starter-kit/init.el")
 ;; (org-babel-load-file "~/emacs/emacs-starter-kit/starter-kit.org")
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(canlock-password "596bc0dc6e2f3391e0f1a381451ca8d297f9abf8")
  '(safe-local-variable-values (quote ((eval sbe "document-config")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-meta-line ((t (:inherit font-lock-comment-face :foreground "Purple")))))
