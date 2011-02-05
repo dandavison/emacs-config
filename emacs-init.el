63ab32b327a4bf84f8cb2bf08c36f49c1a4bac6f
Dan Davison <dandavison7@gmail.com>
2011-02-05 13:04:26 +0000

    Include work-specific config file

--- emacs-init.el
+++ emacs-init.el
@@ -5,6 +5,7 @@
 (require 'org-install)
 (require 'org)
 (org-babel-load-file "~/config/emacs/emacs.org")
+(org-babel-load-file "~/Work/Emacs/counsyl.org")
 
 ;; (add-to-list 'load-path "~/emacs/emacs-starter-kit")
 ;; (load-file "~/emacs/emacs-starter-kit/init.el")
@@ -16,9 +17,3 @@
  ;; If there is more than one, they won't work right.
  '(canlock-password "596bc0dc6e2f3391e0f1a381451ca8d297f9abf8")
  '(safe-local-variable-values (quote ((eval sbe "document-config")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-meta-line ((t (:inherit font-lock-comment-face :foreground "Purple")))))
