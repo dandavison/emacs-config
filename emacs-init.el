27f1e3dad3160965d2dc2c453ef9dea6b219f6e1
Dan Davison <dandavison7@gmail.com>
2011-04-04 15:09:52 -0700

    Use dotfiles config file; remove customize code

--- emacs-init.el
+++ emacs-init.el
@@ -5,15 +5,8 @@
 (require 'org-install)
 (require 'org)
 (org-babel-load-file "~/config/emacs/emacs.org")
-(org-babel-load-file "~/Work/Emacs/counsyl.org")
+(org-babel-load-file "~/dotfiles/davison/emacs_davison/counsyl.org")
 
 ;; (add-to-list 'load-path "~/emacs/emacs-starter-kit")
 ;; (load-file "~/emacs/emacs-starter-kit/init.el")
 ;; (org-babel-load-file "~/emacs/emacs-starter-kit/starter-kit.org")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(canlock-password "596bc0dc6e2f3391e0f1a381451ca8d297f9abf8")
- '(safe-local-variable-values (quote ((eval sbe "document-config")))))
