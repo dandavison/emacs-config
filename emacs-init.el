9d2feb7b77b709db7b3e8ddd85da30ea1703189f
Dan Davison <dandavison7@gmail.com>
2011-08-06 11:24:09 -0700

    Clean out cruft from init file

--- emacs-init.el
+++ emacs-init.el
@@ -7,6 +7,22 @@
 (org-babel-load-file "~/config/emacs/emacs.org")
 (org-babel-load-file "~/dotfiles/davison/emacs_davison/counsyl.org")
 
-;; (add-to-list 'load-path "~/emacs/emacs-starter-kit")
-;; (load-file "~/emacs/emacs-starter-kit/init.el")
-;; (org-babel-load-file "~/emacs/emacs-starter-kit/starter-kit.org")
+;; ;; (add-to-list 'load-path "~/emacs/emacs-starter-kit")
+;; ;; (load-file "~/emacs/emacs-starter-kit/init.el")
+;; ;; (org-babel-load-file "~/emacs/emacs-starter-kit/starter-kit.org")
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(canlock-password "c0f183afebc9e2c0b20627e301c66409604d7c1d")
+;;  '(csv-separators (quote (",")))
+;;  '(safe-local-variable-values (quote ((eval sbe "document-config") (noweb-default-code-mode . R-mode) (outline-minor-mode))))
+;;  '(session-use-package t nil (session)))
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(default ((t (:inherit nil :stipple nil :background "#232323" :foreground "#E6E1DC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "Monaco"))))
+;;  '(magit-section-title ((t nil))))
