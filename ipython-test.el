f6946d8affb443d7d1be231dd11940acc4e7bda5
Dan Davison <dandavison7@gmail.com>
2012-03-11 11:14:16 -0700

    Update ipython test config

--- ipython-test.el
+++ ipython-test.el
@@ -8,7 +8,7 @@
  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
  python-shell-completion-setup-code
    "from IPython.core.completerlib import module_completion"
- python-shell-module-completion-string-code
+ python-shell-completion-module-string-code
    "';'.join(module_completion('''%s'''))\n"
  python-shell-completion-string-code
    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
