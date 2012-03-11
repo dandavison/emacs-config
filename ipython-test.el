e59b1d703eeab513b8799276069d3fd949029f71
Dan Davison <dandavison7@gmail.com>
2012-03-11 11:14:06 -0700

    Add ipython test config

---
+++ ipython-test.el
@@ -0,0 +1,15 @@
+(add-to-list 'load-path "~/lib/emacs/python.el")
+(require 'python)
+
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args ""
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-module-completion-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
