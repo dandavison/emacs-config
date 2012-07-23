c8146dc82740db1044dce9cab39896494844c7e8
Dan Davison <dandavison7@gmail.com>
2012-07-23 12:14:52 -0700

    Non-public additions to emacs-init

--- emacs-init.el
+++ emacs-init.el
@@ -7,5 +7,33 @@
 (require 'org-install)
 (require 'org)
 
+
+(add-to-list 'load-path "~/projects/counsyl.el")
+(require 'counsyl)
+(defalias 'dan/find 'counsyl/grep)
+(defalias 'dan/register-key-bindings 'counsyl/register-key-bindings)
+(defalias 'dan--set-key-bindings 'counsyl--set-key-bindings)
+(defvaralias 'dan/key-bindings 'counsyl/key-bindings)
+(defun dan/python-import ()
+  (interactive)
+  (let ((import (counsyl/import)))
+    (when import
+      (python-shell-send-string-no-output import))))
+
+(counsyl/register-key-bindings
+ '(global-map .
+              (("\C-c\M-g" . counsyl/grep))))
+(require 'python)
+(counsyl/register-key-bindings
+ '("python" .
+   (("\C-c\M-i" . counsyl/import)
+    ([(super o)] . counsyl/open-in-github))))
+
+(counsyl/register-key-bindings
+ '("inferior-python" .
+   (("\C-c\M-i" . counsyl/import))))
+
+
+
 (org-babel-load-file "~/config/emacs/emacs.org")
 (load-file "~/config/emacs/extra.el")
