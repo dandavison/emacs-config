0496fecb2d589442865bdf6cc9fd4b3fd3026718
Dan Davison <dandavison7@gmail.com>
2012-09-22 11:16:16 -0700

    .

--- emacs-init.el
+++ emacs-init.el
@@ -13,7 +13,7 @@
 (defvaralias 'dan/key-bindings 'counsyl/key-bindings)
 (defun dan/python-import ()
   (interactive)
-  (let ((import (counsyl/import)))
+  (let ((import (counsyl/python-import)))
     (when import
       (python-shell-send-string-no-output import))))
 
@@ -23,15 +23,29 @@
 (require 'python)
 (counsyl/register-key-bindings
  '("python" .
-   (("\C-c\M-i" . counsyl/import)
+   (("\C-c\M-i" . counsyl/python-import)
     ([(super o)] . counsyl/open-in-github))))
 
 (counsyl/register-key-bindings
  '("inferior-python" .
-   (("\C-c\M-i" . counsyl/import))))
+   (("\C-c\M-i" . counsyl/python-import))))
 
 
 (setq org-babel-load-languages '((emacs-lisp . t) (R . t)))
 (setq org-confirm-babel-evaluate nil)
 (org-babel-load-file "~/config/emacs/emacs.org")
 (load-file "~/config/emacs/extra.el")
+
+
+
+;; Sort out this mess
+
+;; python -W ignore:Module:UserWarning
+
+(setq dan/ipython-command "~/website/counsyl/product/manage.py shellplus")
+
+(rope-open-project "~/website/build/site-packages")
+;; (rope-open-project "~/seq_pipeline/")
+
+(setq ffip-project-root "~/website/counsyl/product")
+(fuzzy-find-project-root "~/website/counsyl/product")
