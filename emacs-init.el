830233bbfcb0162f125f9c0c852cc7bf3f2b3f69
Dan Davison <dan@counsyl.com>
2013-01-19 15:24:07 -0800

    org-src face hacks

--- emacs-init.el
+++ emacs-init.el
@@ -40,3 +40,17 @@
 (setq org-confirm-babel-evaluate nil)
 (org-babel-load-file "~/config/emacs/emacs.org")
 (load-file "~/config/emacs/extra.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-block-begin-line ((t (:inherit org-meta-line :foreground "gray93"))) t)
+ '(org-block-end-line ((t (:inherit org-meta-line :foreground "gray93"))) t)
+ '(org-document-info-keyword ((t (:inherit org-meta-line :foreground "gray93")))))
