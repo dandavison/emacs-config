e62d3f6448a08046e4eb24c1dae6a6b6551bff80
Dan Davison <dan@counsyl.com>
2014-10-11 17:45:25 -0700

    .

--- emacs-init.el
+++ emacs-init.el
@@ -55,6 +55,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "gray7" :foreground "#F8F8F8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 130 :width normal :foundry "apple" :family "Menlo"))))
  '(comint-highlight-prompt ((t (:foreground "gold"))))
  '(flymake-errline ((t (:background "dark blue" :foreground "black"))))
  '(org-block-begin-line ((t (:inherit org-meta-line :foreground "black"))) t)
