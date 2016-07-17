4cdf0d9658ccc7eb6fcaca6cc8de998be3187e5d
Dan Davison <dandavison7@gmail.com>
2016-07-16 17:35:29 -0700

    Revert to normal ipdb breakpoint

--- lib.el
+++ lib.el
@@ -469,7 +469,7 @@ With C-u prefix argument copy URL to clipboard only."
      (format
       (if traceback
           "import traceback ; import %s ; print(traceback.format_exc()) ; %s.set_trace()"
-        (if nil
+        (if t
             "import %s ; %s.set_trace()"
           "import IPython; IPython.embed(banner1='')"))
       debugger debugger))))
