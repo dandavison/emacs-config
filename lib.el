8e9e0a115c8fe14608ee6501d76d8ace96811c1d
Dan Davison <dandavison7@gmail.com>
2017-04-13 22:30:41 -0700

    comment-or-uncomment-region-or-line
    http://stackoverflow.com/a/9697222/583763

--- lib.el
+++ lib.el
@@ -180,6 +180,15 @@
       (complete-symbol nil)
     (indent-for-tab-command)))
 
+;;; Comment
+(defun comment-or-uncomment-region-or-line ()
+    "Comments or uncomments the region or the current line if there's no active region."
+    (interactive)
+    (let (beg end)
+        (if (region-active-p)
+            (setq beg (region-beginning) end (region-end))
+            (setq beg (line-beginning-position) end (line-end-position)))
+        (comment-or-uncomment-region beg end)))
 
 ;;; Bookmarks
 
