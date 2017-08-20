444b6fd43579cc79f380911374e7a21a7863001a
Dan Davison <dandavison7@gmail.com>
2017-08-20 21:30:14 +0200

    Fix helm ignore paths

--- lib.el
+++ lib.el
@@ -948,12 +948,16 @@ If LIST is nil use `projectile-project-root-parent-directories'"
 
 
 ;;; Helm
-(setq dan/ignored-patterns '("*.sql" "*.wsdl" "*.js" "*.css" "*.pdf" "*/migrations/*" "*.scss" ".svg"))
+(setq dan/ignored-patterns '("*.sql" "*.wsdl" "*.js.min" "*.css.min" "*.pdf" "*/migrations/*" ".svg"))
+
+;; Originals
+;; (setq helm-grep-ignored-files '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo"))
+;; (setq grep-find-ignored-files '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo"))
+;; (setq helm-grep-git-grep-command "git --no-pager grep -n%cH --color=always --exclude-standard --no-index --full-name -e %p -- %f")
 
 (setq helm-grep-ignored-files (append helm-grep-ignored-files dan/ignored-patterns))
 (setq grep-find-ignored-files (append grep-find-ignored-files dan/ignored-patterns))
 
-;; (setq helm-grep-git-grep-command "git --no-pager grep -n%cH --color=always --exclude-standard --no-index --full-name -e %p -- %f")
 (setq helm-grep-git-grep-command
       (format "%s './*' %s"
               helm-grep-git-grep-command
@@ -972,7 +976,7 @@ If LIST is nil use `projectile-project-root-parent-directories'"
     (if t
         (let ((helm-projectile-set-input-automatically t)
               (grep-find-ignored-files
-               (append grep-find-ignored-files dan/projectile-ignored-files)))
+               (append grep-find-ignored-files dan/ignored-patterns)))
           (helm-projectile-grep))
       (counsel-git-grep nil (thing-at-point 'symbol)))))
 
@@ -985,7 +989,7 @@ If LIST is nil use `projectile-project-root-parent-directories'"
                                      (error "You're not in a project")))
                    (helm-projectile-set-input-automatically nil)
                    (grep-find-ignored-files
-                    (append grep-find-ignored-files dan/projectile-ignored-files)))
+                    (append grep-find-ignored-files dan/ignored-patterns)))
                (helm-projectile-grep-or-ack project-root))) dir))
 
 
