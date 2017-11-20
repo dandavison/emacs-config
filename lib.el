0c1bc0c60451f8823299322354bdfc9b0cf1e36c
Dan Davison <dandavison7@gmail.com>
2017-11-20 13:43:39 -0500

    Exclude tests from helm searches

--- lib.el
+++ lib.el
@@ -1147,7 +1147,7 @@ If LIST is nil use `projectile-project-root-parent-directories'"
 
 
 ;;; Helm
-(setq dan/ignored-patterns '("*.sql" "*.wsdl" "*.js.min" "*.min.js" "*.css.min" "*.pdf" "*/migrations/*" ".svg"))
+(setq dan/ignored-patterns '("*.sql" "*.wsdl" "*.js.min" "*.min.js" "*.css.min" "*.pdf" "*/migrations/*" ".svg" "*vendor/*" "*/tests/*" "*/test_*.py"))
 
 ;; Originals
 (setq dan/helm-grep-ignored-files-orig '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo"))
