f39935c24a78302ec2144f6fa3410aba00b4b989
Dan Davison <dandavison7@gmail.com>
2017-11-24 15:41:50 -0500

    helm: don't overwrite variable from library

--- lib.el
+++ lib.el
@@ -1152,14 +1152,14 @@ If LIST is nil use `projectile-project-root-parent-directories'"
 ;; Originals
 (setq dan/helm-grep-ignored-files-orig '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo"))
 (setq dan/grep-find-ignored-files-orig '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo"))
-;; (setq helm-grep-git-grep-command "git --no-pager grep -n%cH --color=always --exclude-standard --no-index --full-name -e %p -- %f")
+(setq dan/helm-grep-git-grep-command-orig "git --no-pager grep -n%cH --color=always --exclude-standard --no-index --full-name -e %p -- %f")
 
 (setq helm-grep-ignored-files (append dan/helm-grep-ignored-files-orig dan/ignored-patterns))
 (setq grep-find-ignored-files (append dan/grep-find-ignored-files-orig dan/ignored-patterns))
 
 (setq helm-grep-git-grep-command
       (format "%s './*' %s"
-              helm-grep-git-grep-command
+              dan/helm-grep-git-grep-command-orig
               (mapconcat (lambda (s) (format "':!%s'" s)) dan/ignored-patterns " ")))
 
 (defun dan/helm-projectile-switch-project (&optional arg)
