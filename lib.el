044ea1155ac399dccdfd7b1eadcbc258eaa72a8f
Dan Davison <dandavison7@gmail.com>
2017-11-27 12:59:24 -0800

    Function to change global ignore-files variables by side effect

--- lib.el
+++ lib.el
@@ -1151,6 +1151,28 @@ If LIST is nil use `projectile-project-root-parent-directories'"
 
 
 ;;; Helm
+
+(defun dan/set-global-ignored-files-variables! (&optional exclude-tests)
+  (interactive)
+  (let ((helm-grep-ignored-files-orig
+         '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo"))
+        (grep-find-ignored-files-orig
+         '(".#*" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo"))
+        (helm-grep-git-grep-command-orig
+         "git --no-pager grep -n%cH --color=always --exclude-standard --no-index --full-name -e %p -- %f")
+        (ignored-patterns
+         (if exclude-tests
+             (-filter (lambda (s) (string-match-p (regexp-quote "test") s))
+                      dan/ignored-patterns)
+           dan/ignored-patterns)))
+    (setq helm-grep-ignored-files (append helm-grep-ignored-files-orig ignored-patterns))
+    (setq grep-find-ignored-files (append grep-find-ignored-files-orig ignored-patterns))
+    (setq helm-grep-git-grep-command
+          (format "%s './*' %s"
+                  helm-grep-git-grep-command-orig
+                  (mapconcat (lambda (s) (format "':!%s'" s)) ignored-patterns " ")))))
+
+
 (defun dan/helm-projectile-switch-project (&optional arg)
   (interactive "P")
   (if arg (projectile-switch-project)
