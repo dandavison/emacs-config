5cd089d38549a23e4bf30f56b7c0ccc80d66a7b6
Dan Davison <dandavison7@gmail.com>
2018-11-03 11:37:59 -0400

    org src block appearance

--- emacs.el
+++ emacs.el
@@ -793,6 +793,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-block ((t (:background "white" :foreground "#000088"))))
+ '(org-block-begin-line ((t (:background "White" :foreground "lightgrey" :underline nil))))
+ '(org-block-end-line ((t (:background "white" :foreground "lightgrey" :overline nil))))
  '(org-level-1 ((t (:foreground "#CC7733" :height 120)))))
 
 (message "⚡")
