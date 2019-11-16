0969956504b383a91cbbcbe24467a85dbafe3cae
Dan Davison <dandavison7@gmail.com>
2019-11-16 10:08:13 -0600

    Minimal config

---
+++ emacs-minimal.el
@@ -0,0 +1,20 @@
+;; Conveniences / Annoyances
+(setq ring-bell-function (lambda nil nil))
+(fset 'yes-or-no-p 'y-or-n-p)
+(setq mac-command-modifier 'super)
+(setq mac-option-modifier 'meta)
+
+;; Full screen
+(setq ns-use-native-fullscreen nil)
+(run-with-timer 1 nil 'toggle-frame-fullscreen)
+
+;; Server
+(require 'server)
+(unless (server-running-p) (server-start))
+
+;; Packages
+(package-initialize)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+(require 'use-package)
+(use-package xenops :load-path "~/src/xenops")
+
